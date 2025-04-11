63e4ecec2886ddb8f2ef212b66084c478a6552cf
RBertini78 <r.bertini@gmail.com>
2025-04-11 17:34:41 -0300

    Iniciando com o EntityFramework Core, criação da base de dados, criação do contexto de dados

--- Back/src/BertiniEventos.API/Controllers/EventoControler.cs
+++ Back/src/BertiniEventos.API/Controllers/EventoControler.cs
@@ -2,6 +2,7 @@ using System;
  using System.Collections.Generic;
  using System.Linq;
  using System.Threading.Tasks;
+using BertiniEventos.API.Data;
 using BertiniEventos.API.Models;
 using Microsoft.AspNetCore.Mvc;
  using Microsoft.Extensions.Logging;
@@ -11,55 +12,24 @@ using Microsoft.AspNetCore.Mvc;
      [ApiController]
      [Route("api/[controller]")]
      public class EventoController : ControllerBase
-     {
-        public IEnumerable<Evento> _evento = new Evento[] 
-             {
-                 new Evento()
-                 {
-                     EventoId = 1,
-                     Local = "São Paulo",
-                     DataEvento = DateTime.Now.AddDays(20).ToString("dd/MM/yyyy"),
-                     Tema = "Aprendendo ASP.NET Core",
-                     QtdPessoas = 250,
-                     Lote = "1º Lote",
-                     ImagemURL = "Sampa.png"
-                 },
-                 new Evento()
-                 {
-                     EventoId = 2,
-                     Local = "Rio de Janeiro",
-                     DataEvento = DateTime.Now.AddDays(45).ToString("dd/MM/yyyy"),
-                     Tema = "Angular e ASP.NET Core",
-                     QtdPessoas = 300,
-                     Lote = "2º Lote",
-                     ImagemURL = "Rio.png"
-                 }
-                 new Evento()
-                 {
-                     EventoId = 3,
-                     Local = "Belo Horizonte",
-                     DataEvento = DateTime.Now.AddDays(50).ToString("dd/MM/yyyy"),
-                     Tema = "Aprendendo ASP.NET Core",
-                     QtdPessoas = 200,
-                     Lote = "1º Lote",
-                     ImagemURL = "BH.png"
-                 }
-             };
-                  
-         public EventoController()
+     {                          
+         private readonly DataContext _context;
+         public EventoController(DataContext context)
          {
+              _context = context;
          }
+         
  
          [HttpGet]
          public IEnumerable<Evento> Get()
          {
-             return _evento;
+             return _context.Eventos;
          }
 
          [HttpGet("{id}")]
-         public IEnumerable<Evento> GetById(int id)
+         public Evento GetById(int id)
          {
-             return _evento.Where(evento => evento.EventoId == id);
+             return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
          }
 
          [HttpPost]
--- Back/src/BertiniEventos.API/Startup.cs
+++ Back/src/BertiniEventos.API/Startup.cs
@@ -2,11 +2,13 @@ using System;
  using System.Collections.Generic;
  using System.Linq;
  using System.Threading.Tasks;
- using Microsoft.AspNetCore.Builder;
+using BertiniEventos.API.Data;
+using Microsoft.AspNetCore.Builder;
  using Microsoft.AspNetCore.Hosting;
  using Microsoft.AspNetCore.HttpsPolicy;
  using Microsoft.AspNetCore.Mvc;
- using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
  using Microsoft.Extensions.DependencyInjection;
  using Microsoft.Extensions.Hosting;
  using Microsoft.Extensions.Logging;
@@ -26,7 +28,7 @@ using System;
          // This method gets called by the runtime. Use this method to add services to the container.
          public void ConfigureServices(IServiceCollection services)
          {
- 
+             services.AddDbContext<DataContext>  (context => context.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
              services.AddControllers();
              services.AddSwaggerGen(c =>
              {
