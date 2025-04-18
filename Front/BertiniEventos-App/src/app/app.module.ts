4dfffa5be98a05607b1d6429bcb89910bf209090
RBertini78 <r.bertini@gmail.com>
2025-04-18 00:29:33 -0300

    Criado botão para exibir/ocultar as imagens com collapse. input com 2way binding. adicionadas imagens

--- Front/BertiniEventos-App/src/app/app.module.ts
+++ Front/BertiniEventos-App/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 import { HttpClient } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import {AppRoutingModule} from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -25,6 +26,7 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   // Removed bootstrap array as AppComponent is a standalone component
--- Front/BertiniEventos-App/src/app/eventos/eventos.component.ts
+++ Front/BertiniEventos-App/src/app/eventos/eventos.component.ts
@@ -2,22 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from "@angular/common/http";
 import { HttpClient } from "@angular/common/http";
+import { CollapseDirective, CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CollapseModule,FormsModule],
   templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.scss'
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  filterList: string = '';
   // Replace 'any' with the appropriate type for your eventos
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos()
+  }
+
+  exibirImg() {
+    this.showImg = !this.showImg;
   }
 
   public getEventos(): void {
